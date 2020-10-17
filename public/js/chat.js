a6b7cfa1a632854a15f5a8576179621709802784
jared james <jaredjames.dev@gmail.com>
2020-10-17 13:32:10 +1300

    finished chat app, added auto scroll

---
+++ public/js/chat.js
@@ -0,0 +1,121 @@
+const socket = io()
+
+// Elements
+const $MessageForm = document.getElementById('myForm')
+const $messageFormInput = document.getElementById('messageFormInput')
+const $messageFormbutton = document.getElementById('messageFormButton')
+const $sendLocation = document.getElementById('sendLocation')
+const $messages = document.getElementById('messages')
+
+// Templates
+const messageTemplate = document.getElementById('message-template').innerHTML
+const locationTemplate = document.getElementById('location-template').innerHTML
+const sidebarTemplate = document.getElementById('sidebar-template').innerHTML
+
+// Options
+const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true })
+
+const autoscroll = () => {
+	// New message element
+	const $newMessage = $messages.lastElementChild
+
+	// Height of the new message
+	const newMessageStyles = getComputedStyle($newMessage)
+	const newMessageMargin = parseInt(newMessageStyles.marginBottom)
+	const newMessageHeight = $newMessage.offsetHeight + newMessageMargin
+
+	// Visible height
+	const visibleHeight = $messages.offsetHeight
+
+	// Height of messages containeer
+	const containerHeight = $messages.scrollHeight
+
+	// How far have i scrolled?
+	const scrollOffset = $messages.scrollTop + visibleHeight
+
+	if (containerHeight - newMessageHeight <= scrollOffset) {
+			$messages.scrollTop = $messages.scrollHeight
+	}
+
+
+}
+
+socket.on('message', (welcome) => {
+	const html = Mustache.render(messageTemplate, {
+		username: welcome.username,
+		message: welcome.text,
+		createdAt: moment(welcome.createdAt).format('h:mm a')
+	})
+	$messages.insertAdjacentHTML('beforeend', html)
+	autoscroll()
+})
+
+socket.on('location', (location) => {
+	console.log(location)
+	const html = Mustache.render(locationTemplate, {
+		username: location.username,
+		location: location.location,
+		createdAt: moment(location.createdAt).format('h:mm a')
+	})
+	$messages.insertAdjacentHTML('beforeend', html)
+	autoscroll()
+
+})
+
+socket.on('roomData', ({ room, users }) => {
+	const html = Mustache.render(sidebarTemplate, {
+		room,
+		users
+	})
+	document.getElementById('sidebar').innerHTML = html
+})
+
+
+$MessageForm.addEventListener('submit', (e) => {
+	e.preventDefault()
+
+	// Disable button
+	$messageFormbutton.setAttribute('disabled', 'disabled')
+
+	const sendMessage = e.target.elements.message.value
+
+	socket.emit('sendMessage', sendMessage, (error) => {
+		// Enable button
+		$messageFormbutton.removeAttribute('disabled')
+		$messageFormInput.value = ''
+		if (error) {
+			return console.log(error)
+		} else {
+			console.log('Message Delivered')
+		}
+	})
+})
+
+$sendLocation.addEventListener('click', () => {
+	if (!navigator.geolocation) {
+		return alert('Geolocation is not supported by your bbrowser')
+	}
+	$sendLocation.setAttribute('disabled', 'disabled')
+
+
+	navigator.geolocation.getCurrentPosition((position) => {
+
+		const location = `https://google.com/maps?q=${position.coords.latitude},${position.coords.longitude}`
+
+		socket.emit('sendLocation', location, (error) => {
+			if (error) {
+				return console.log(error)
+			} else {
+				console.log('Location sent!')
+				$sendLocation.removeAttribute('disabled')
+			}
+		})
+	})
+})
+
+socket.emit('join', { username, room }, (error) => {
+	if (error) {
+		alert(error)
+		location.href = '/'
+	}
+})
--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-const express = require('express')
 const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
+const Filter = require('bad-words')
+const { generateMessage, generateLocationMessage } = require('./utils/messages')
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./utils/users')
+
+
 const app = express()
+// Creates server outside of the express library
+const server = http.createServer(app)
+// Socket IO expects to be called with raw HTTP server
+const io = socketio(server)
+
 const port = process.env.PORT || 3000
 
 
@@ -10,7 +22,69 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 // Serve up the public directory
 app.use(express.static(publicDirectoryPath))
 
+const welcome = "Welcome!"
 
-app.listen(port, () => {
+// new client connecting - socket is an object
+io.on('connection', (socket) => {
+  console.log('New Websocket connection')
+
+  // sends an event from the server
+  socket.on('join', ({ username, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, username, room })
+
+    if (error) {
+      return callback(error)
+    }
+
+    socket.join(user.room)
+
+    socket.emit('message', generateMessage('Admin','Welcome!'))
+    socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`))
+    io.to(user.room).emit('roomData', {
+      room: user.room,
+      users: getUsersInRoom(user.room)
+    })
+    callback()
+  })
+
+  socket.on('sendMessage', (sendMessage, callback) => {
+    const myUser = getUser(socket.id)
+    const filter = new Filter()
+    if (filter.isProfane(sendMessage)) {
+      return callback('Profanity is not allowed')
+    }
+
+    io.to(myUser.room).emit('message', generateMessage(myUser.username, sendMessage))
+    callback()
+  })
+
+  socket.on('sendLocation', (location, callback) => {
+    const myUser = getUser(socket.id)
+    if (!location) {
+      return callback('Error finding location')
+    }
+    io.to(myUser.room).emit('location', generateLocationMessage(myUser.username, location))
+    callback()
+  })
+
+
+  // runs when a client is disconnected
+  socket.on('disconnect', () => {
+    const user = removeUser(socket.id)
+
+    if (user) {
+      io.to(user.room).emit('message', generateMessage(`${user.username} has left the room`))
+      io.to(user.room).emit('roomData', {
+        room: user.room,
+        users: getUsersInRoom(user.room)
+      })
+    }
+
+  })
+
+})
+
+server.listen(port, () => {
   console.log(`Server is up on port ${port}`)
 })
+
